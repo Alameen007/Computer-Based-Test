69447265fbe4bbaaac99d19bb83672b4b67c2044
Alameen007 <alaminabdulkareem@gmail.com>
2017-06-07 16:22:52 +0100

    downgrading my packages

--- app/components/subjects/mathematics/Result.jsx
+++ app/components/subjects/mathematics/Result.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var {connect} = require('react-redux');
+import {Link} from 'react-router';
 var actions = require('actions');
 
 import MathQuestion from 'MathQuestion';
@@ -7,29 +8,13 @@ import MathQuestion from 'MathQuestion';
 export class Result extends React.Component {
 
 // var selected = e.target.value;
-getScore(e) {
-  e.preventDefault();
-  var {id, mathematics, mathQuestion, mathText, answers, dispatch} = this.props;
-  var answersList = [];
-  $(".form").each(function(){
-    var answer = $(':radio:checked').value;
 
-    if (answer !== undefined) {
-        answersList.push({
-          answer: answer
-        });
-      }
-console.log(answersList);
-  });
-
-}
   render() {
     var {id, mathematics, mathQuestion, mathText, answers, dispatch} = this.props;
     return (
       <div >
-        <form onSubmit={this.getScore.bind(this)}>
-          <button id="vResult" className="button expanded resultB"  disabled>View Results<ebuttonw
-        </form> Result/>
+
+        <Link to="/MathScores">  <button id="vResult" className="button expanded resultB"  disabled>View Result</button></Link>
       </div>
     )
   }
